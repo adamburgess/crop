0661760733591926199b1fea9ac5188fb47185ec
Adam Burgess <adamburgess@users.noreply.github.com>
2021-03-17 23:49:36 +1100

    Yet more snowpack/rollup trials.

---
+++ frontend5/projects/crop/index.tsx
@@ -0,0 +1,13 @@
+import { h, render } from 'preact';
+import 'preact/devtools';
+//import styles from './index.module.css'
+
+const root = document.getElementById('root')
+
+function App() {
+    return <div class={"foo"}>Crop - Hi!</div>
+}
+
+if (root) {
+    render(<App />, root);
+}
