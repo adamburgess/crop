699ccd166d3f6eb1b59299a2e52b6bc7777e34d8
Adam Burgess <adamburgess@users.noreply.github.com>
2022-09-10 21:10:49 +1000

    postcss config can be esm

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// const tailwindcss = require('tailwindcss');
+
+// module.exports = {
+//     plugins: [
+//         tailwindcss()
+//     ],
+// };
+
+import tailwindcss from 'tailwindcss'
+export default {
+    plugins: [
+        tailwindcss()
+    ]
+}
